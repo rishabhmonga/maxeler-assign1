9aa8824c812ea530c16c52ddcd6fae5ebdd62f6f
rishabhmonga <rishabh.tar.gz@gmail.com>
2017-05-29 16:24:15 -0700

    Adding Documentation

--- CPUCode/svd.c
+++ CPUCode/svd.c
@@ -49,10 +49,10 @@ void printVector(double *v, int size) {
 // double svd(double a, double b);
 // double svd(double a, double b) {
 //     double absa,absb;
-
+//
 //     absa = fabs(a);
 //     absb = fabs(b);
-
+//
 //     if(absa > absb)
 //         return(absa * sqrt(1.0 + SQR(absb/absa)));
 //     else
